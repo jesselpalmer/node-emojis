8a639f5791ef0b79d2a306d85af3341ea85ab3a1
Jesse Palmer <jesselpalmer@gmail.com>
2019-08-17 11:44:58 -0700

    fix: travel emojis now added

--- test/test.js
+++ test/test.js
@@ -4,7 +4,23 @@ const expect = require('chai').expect
 const emojis = require('../lib/emojis')
 
 describe('emojis', () => {
-  it('should return a successful emoji', () => {
-    expect(emojis.boar).to.equal('🐗')
+  it('should return a successful animal emoji', () => {
+    expect(emojis.cat).to.equal('🐈')
+  })
+
+  it('should return a successful people emoji', () => {
+    expect(emojis.grin).to.equal('😁')
+  })
+
+  it('should return a successful travel emoji', () => {
+    expect(emojis.snow_capped_mountain).to.equal('🗻')
+  })
+
+  it('should return a successful food emoji', () => {
+    expect(emojis.melon).to.equal('🍈')
+  })
+
+  it('should return a successful nature emoji', () => {
+    expect(emojis.lit).to.equal('🔥')
   })
 })
