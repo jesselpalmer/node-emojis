15590eff0293d9b94662991c1e16df6612d0f8fc
Jesse Palmer <jesselpalmer@gmail.com>
2021-04-04 12:54:13 -0700

    feat: add script to fetch emojis

---
+++ scripts/get-emojis.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer')
+
+const getEmojiName = emojiNameRawStr => {
+  return emojiNameRawStr.replace(/\W/g, ' ')
+                        .replace(/  /g, ' ')
+                        .replace(/ /g, '_')
+                        .replace(/__/g, '_')
+                        .toLowerCase()
+}
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://emojipedia.org/emoji-1.0/')
+  const emojisObject = {}
+  
+  const emojis = await page.$$eval('li', emojis => emojis.map(emojiElement => {
+    const emojiStr = emojiElement.textContent
+    const emoji =  emojiStr.split(' ')[0].trim()
+    const emojiNameRawStr = emojiStr.split(' ')
+                              .splice(1)
+                              .join(' ')
+    const emojiName = getEmojiName(emojiNameRawStr)
+
+    return [emoji, emojiName]
+  }))
+
+  for (let index in emojis) {
+    const currentEmojiArray = emojis[index]
+
+    if (currentEmojiArray[0] !== '' || currentEmojiArray[0] !== ' ' || currentEmojiArray[0] !== 'Emoji') {
+      emojisObject[currentEmojiArray[1]] = currentEmojiArray[0]
+    }
+  }
+
+  console.log(JSON.stringify(emojisObject))
+  await browser.close();
+})()
+
+module.exports = getEmojiName
---
+++ scripts/get-emojis.test.js
@@ -0,0 +1,18 @@
+const getEmojiName = require('./get-emojis')
+
+describe('emojis', () => {
+  test('should lowercase emoji name', () => {
+    const emojiNameRawStr = 'CAT'
+    expect(getEmojiName(emojiNameRawStr)).toMatch('cat')
+  })
+
+  test('should replace hypen with underscore', () => {
+    const emojiNameRawStr = 'smiley-face'
+    expect(getEmojiName(emojiNameRawStr)).toMatch('smiley_face')
+  })
+
+  test('should replace double hypens with a single underscore', () => {
+    const emojiNameRawStr = 'smiley--face'
+    expect(getEmojiName(emojiNameRawStr)).toMatch('smiley_face')
+  })
+})
